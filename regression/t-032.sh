75ac23878ba3ad90a1f72fb63f3504a422a2c224
Frediano Ziglio <frediano.ziglio@cloud.com>
2024-09-28 12:28:05 +0100

    Fix some erros inserting patches
    During "guilt import" we need to insert patch name into series
    file. The code was having some issues:
    - patch comments cause the match to fail and the new name
      not being inserted;
    - space before or after top patch name were not ignored.
    Fixed these issues and added a regression test.
    
    Signed-off-by: Frediano Ziglio <frediano.ziglio@cloud.com>

--- regression/t-032.sh
+++ regression/t-032.sh
@@ -7,6 +7,14 @@ source "$REG_DIR/scaffold"
 
 cmd setup_repo
 
+# add a comment and start spaces to last patch in the series
+# to detects a bug inserting patches in the series
+sed 's,^mode,  mode # comment,' < .git/patches/master/series > .git/patches/master/series.tmp
+mv .git/patches/master/series.tmp .git/patches/master/series
+cmd grep ' mode ' .git/patches/master/series
+
+cmd guilt push --all
+
 cmd touch foo foo:baz
 
 # invalid character
@@ -21,14 +29,23 @@ shouldfail guilt import -P foo foo2
 # ok
 cmd guilt import -P foo3 foo
 
+# this should contain foo3
+cmd guilt series
+
 # duplicate patch name
 shouldfail guilt import -P foo3 foo
 
 # ok
 cmd guilt import -P foo2 foo
 
+# this should contain foo2
+cmd guilt series
+
 # ok
 cmd guilt import foo
+
+# this should contain foo
+cmd guilt series
 
 # duplicate patch name (implicit)
 shouldfail guilt import foo
