f8e69f0f8b0c215cc005eb01135f9fd69e1559a0
Frediano Ziglio <freddy77@gmail.com>
2026-03-30 14:08:39 +0100

    Update script for release
    Check for errors.
    Improve version string check.
    Patch automatically guilt file for version.
    Create just annotates tags.

--- scripts/release_version.sh
+++ scripts/release_version.sh
@@ -1,34 +1,37 @@
 #!/bin/bash
 
+set -e
+
 if [ $# -ne 1 ]; then
 	echo "Usage: $0 <tagname>"
 	exit 1
 fi
 
-case "$1" in
-	v*)
-		tag="$1"
-		;;
-	[0-9]*)
-		tag="v$1"
-		;;
-esac
-
-ver=`echo $tag | sed -e 's/^v//'`
+if [[ "$1" =~ ^v?([0-9][.0-9]+)(-rc[1-9][0-9]*)?$ ]]; then
+	ver="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
+else
+	echo "Wrong version specified $1" >&2
+	exit 1
+fi
+tag="v$ver"
 
 echo "About start release process of '`git rev-parse HEAD`' as '$tag'..."
 echo "Press enter to continue."
 read n
 
-echo "1) Edit GUILT_VERSION in 'guilt'"
+echo "1) Patching GUILT_VERSION in 'guilt'"
 read n
-vim guilt
-git update-index guilt
+sed -i "s,^GUILT_VERSION=\".*\"$,GUILT_VERSION=\"$ver\"," guilt
+if ! grep -x -q "GUILT_VERSION=\"$ver\"" guilt; then
+	echo "Version in guilt file was not updated correctly" >&2
+	exit 1
+fi
+git add guilt
 git commit -s -m "Guilt $tag"
 
 echo "2) Tag the commit with '$tag'"
 read n
-git tag -u C7958FFE -m "Guilt $tag" "$tag"
+git tag -a -m "Guilt $tag" "$tag"
 
 echo "3) Generate guilt-$ver.tar.gz"
 read n
